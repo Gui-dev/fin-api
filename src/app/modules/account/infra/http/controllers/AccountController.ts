220ec5672bbe5cfc6be96a15aa904b775151c62f
Gui-dev <adrianon10@gmail.com>
2021-12-07 03:43:01 -0300

    feat: add helper method to list accounts

--- src/app/modules/account/infra/http/controllers/AccountController.ts
+++ src/app/modules/account/infra/http/controllers/AccountController.ts
@@ -4,7 +4,14 @@ import { v4 as uuid } from 'uuid'
 import { AccountService } from '@modules/account/services/AccountService'
 
 export class AccountController {
-  public async create (request: Request, response: Response) {
+  public async index (request: Request, response: Response): Promise<Response> {
+    const accountService = new AccountService()
+    const accounts = await accountService.listAllAccounts()
+
+    return response.status(201).json(accounts)
+  }
+
+  public async create (request: Request, response: Response): Promise<Response> {
     const { name, cpf } = request.body
     const accountService = new AccountService()
 
--- src/app/modules/account/services/AccountService.ts
+++ src/app/modules/account/services/AccountService.ts
@@ -6,10 +6,41 @@ type AccountDTO = {
 }
 
 export class AccountService {
-  public async createAccount (data: AccountDTO): Promise<AccountDTO[]> {
-    const customers = []
-    customers.push(data)
+  public customers: Array<AccountDTO>
 
-    return customers
+  constructor () {
+    this.customers = [{
+      id: 'ef6f2b81-f2ca-44b5-b914-59d40d8dfd57',
+      name: 'Clark Kent',
+      cpf: '98754326787',
+      statement: []
+    }]
+  }
+
+  public async createAccount (data: AccountDTO): Promise<AccountDTO[]> {
+    const customerAlreadyExists = this.customers.some(customer => customer.cpf === data.cpf)
+
+    if (customerAlreadyExists) {
+      throw new Error('Customer already exists')
+    }
+
+    this.customers.push(data)
+
+    return this.customers
+  }
+
+  public async listAllAccounts (): Promise<AccountDTO[]> {
+    console.log(this.customers)
+    return this.customers
+  }
+
+  public async listByCpf (cpf: string): Promise<AccountDTO> {
+    const customer = this.customers.find(customer => customer.cpf === cpf)
+
+    if (!customer) {
+      throw new Error('Customer not found')
+    }
+
+    return customer
   }
 }
