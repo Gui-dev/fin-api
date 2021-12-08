29120c915a316cfb53ad14de482385832e86db73
Gui-dev <adrianon10@gmail.com>
2021-12-08 03:37:24 -0300

    feat: add new route

--- src/app/modules/account/infra/http/routes/account.routes.ts
+++ src/app/modules/account/infra/http/routes/account.routes.ts
@@ -2,17 +2,20 @@ import { Router } from 'express'
 
 import { AccountController } from '@modules/account/infra/http/controllers/AccountController'
 import { DespositController } from '@modules/account/infra/http/controllers/DepositController'
+import { WithDrawController } from '@modules/account/infra/http/controllers/WithDrawController'
 
 const routesAccount = Router()
 
 const accountController = new AccountController()
 const despositController = new DespositController()
+const withDrawController = new WithDrawController()
 
 routesAccount.get('/', accountController.index)
 routesAccount.get('/statement', accountController.show)
 
 routesAccount.post('/', accountController.create)
 routesAccount.post('/deposit', despositController.create)
+routesAccount.post('/withdraw', withDrawController.create)
 
 export {
   routesAccount
